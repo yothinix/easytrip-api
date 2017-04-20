7437960b1800bb8b65808a98c5f9d2c3f3bc2828
yothinix <m@yothinix.com>
2017-04-20 22:14:11 +0700

    update data store :rocket:

--- data_store.py
+++ data_store.py
@@ -1,31 +1,117 @@
 from box import Box
 
-REVIEW = {'id': 1, 'comment': 'Banana is a fruit, Apple also a fruit. Both are fruit naja'}
-
-ATTRACTION = {
-    'id': 1,
-    'name': 'Sriracha Tiger Zoo',
-    'rating': [1, 5, 2, 4, 0, 5],
-    'address': '341 ตำบล หนองขาม Amphoe Si Racha. Chang Wat Chon Buri 20110',
-    'details': """
-        The Sriracha Tiger Zoo is a zoo in Sri Racha, a city on the outskirts of Pattaya, a seaside city in Chonburi Province. Thailand.
-        It is about 97 km (60 mi) from Bangkok. The zoo claims a population of 200 tigers and around 10,000... crocodiles. the largest
-        such populations in the world.[citation needed] Admission in 2009 (2552) was THB350 for foreigners and THB120 for Thais.
-        """,
-    'opening hours': '8AM - 6PM',
-    'phone': '038 296 556',
-    'photos': [
-        'link1',
-        'link2',
-        'link3'
-    ],
-    'reviews': [REVIEW, REVIEW, REVIEW],
-}
-
 DATA_STORE = Box({
     'attraction': [
-        ATTRACTION,
-        ATTRACTION
+        {
+            'id': 1,
+            'name': 'Sriracha Tiger Zoo',
+            'rating': 4,
+            'address': '341 ตำบล หนองขาม Amphoe Si Racha. Chang Wat Chon Buri 20110',
+            'details': """
+                The Sriracha Tiger Zoo is a zoo in Sri Racha, a city on the outskirts of Pattaya, a seaside city in Chonburi Province. Thailand.
+                It is about 97 km (60 mi) from Bangkok. The zoo claims a population of 200 tigers and around 10,000... crocodiles. the largest
+                such populations in the world.[citation needed] Admission in 2009 (2552) was THB350 for foreigners and THB120 for Thais.
+            """,
+            'opening hours': '8AM - 6PM',
+            'phone': '038 296 556',
+            'photos': [],
+            'reviews': [],
+        },
+        {
+            'id': 2,
+            'name': 'Vajiravudh Scout Camp',
+            'rating': 4.2,
+            'address': 'Bang Phra, Si Racha District, Chon Buri 20110',
+            'details': """
+            ค่ายลูกเสือวชิราวุธ เป็นค่ายลูกเสือแห่งชาติ ตั้งอยู่ที่ ตำบลบางพระ อำเภอศรีราชาจังหวัดชลบุรี ด้านหน้าของค่ายอยู่ติดกับถนนสุขุมวิท 
+ระหว่าง กม. 115-116 จากกรุงเทพมหานคร และจากถนนสุขุมวิท มีถนนเล็กชื่อถนนจักกะพากตัดผ่านริมสนามหญ้าอันกว้างใหญ่เข้าไปยังศาลา
+อำนวยการ และบริเวณส่วนใหญ่ของค่ายลูกเสือวชิราวุธมองเข้าไปจากถนนสุขุมวิท จะเห็นพระบรมราชานุสาวรีย์พระบาทสมเด็จพระมงกุฎเกล้าเจ้า
+อยู่หัว ออกแบบโดยกรมศิลปากรและหล่อด้วยโลหะขนาดหนึ่งเท่าครึ่งพระองค์ ประทับยืนทรงเครื่องแบบนายพลเสือป่าเต็มยศประดิษฐานอยู่ใน
+บริเวณสนามหญ้าอันกว้างใหญ่นั้น และทางด้านหลังของพระบรมราชานุสาวรีย์ฯ จะเห็นธงของค่ายลูกเสือวชิราวุธสบัดพริ้วอยู่บนยอดเขาซากแขก 
+ซึ่งช่วยให้พระบรมราชานุสาวรีย์ดูเด่นและสง่างามยิ่งขึ้น
+            """,
+            'opening hours': '8AM - 6PM',
+            'phone': '038 311 132',
+            'photos': ['https://lh6.googleusercontent.com/-01uRnhi2HT0/V94HaFg6guI/AAAAAAAAFe4/xB97B0ino-Udv54ckVLjXYeV3eOqW-B_QCJkC/s1600-w2000/'],
+            'reviews': [],
+        },
+        {
+            'id': 3,
+            'name': 'Cartoon Network Amazone',
+            'rating': 4.3,
+            'address': '888 Moo 8 NaJomtien, Najomtien, Amphoe Sattahip, Chang Wat Chon Buri 20250',
+            'details': """
+            Cartoon Network Amazone Waterpark is the world's first Cartoon Network themed waterpark located at 888 Moo 8, Na Jomtien, Sattahip, Chonburi, Thailand, 15 kilometers away from South Pattaya.
+            """,
+            'opening hours': '10AM – 7PM',
+            'phone': '033 004 999',
+            'photos': [],
+            'reviews': [],
+        },
+        {
+            'id': 4,
+            'name': 'Koh Loy Park',
+            'rating': 4.2,
+            'address': 'Si Racha District, Chon Buri 201100',
+            'details': """
+            Koh Loy is an attraction at Sriracha, approximately 30 kilometers from Pattaya City. Koh Loy is a very small island connected to the mainland of Sriracha by a long bridge. Although the island is small, it is a good place to visit. The evening time on Koh Loy is very popular for relaxation. The transportation from the main land to Koh Loy is also comfortable for visitors due to the bridge and a car park. Additionally, there are several famous restaurants on this small island.
+            """,
+            'opening hours': '6AM–10PM',
+            'phone': '095 062 7982',
+            'photos': [],
+            'reviews': [],
+        },
+        {
+            'id': 5,
+            'name': 'Surasak Montri Public Park',
+            'rating': 4.3,
+            'address': 'Si Racha District, Chon Buri',
+            'details': """
+            """,
+            'opening hours': '4AM–9PM',
+            'phone': '',
+            'photos': [],
+            'reviews': [],
+        },
+        {
+            'id': 6,
+            'name': 'อ่าวอุดม',
+            'rating': 4.8,
+            'address': 'Surasak, Si Racha District, Chon Buri 20110',
+            'details': """
+            อ่าวอุดม เป็นชุมชนเก่าแก่มีการตั้งรกรากมานาน แต่เริ่มมีประชากรมาอาศัยหนาแน่น ราวพ.ศ. 2450-พ.ศ. 2485 โดยจะมีครอบครัวดั้งเดิมที่อาศัยอยู่ริมทะเล ครอบครัวคนจีนที่มาโล้สำเภามาตั้งรกรากอยู่กันเป็นกงสีที่หนองกระสือ และครอบครัวดั้งเดิมอีกครอบครัวตั้งอยู่ท้ายหนองกระสือ คือ ครอบครัวหมอสด เดิม อ่าวอุดม เรียกว่า อ่าวกระสือ เพราะ สมัยก่อนมีหนองน้ำขนาดใหญ่ ชื่อ หนองกระสือ ซึ่งโดยบริเวณนั้นเป็นที่ดินของกงสีของนายลู่ แซ่เจ็ง กับ นางอิ้ว แซ่ลิ้ม และพื้นที่ของบริษัทเคอรี่ สยามซีพอร์ต ปัจจุบันหนองกระสือไม่มีแล้ว เพราะโดนถมกลายเป็นที่ตั้ง โกดังบริษัทเคอรรี่ สยามซีพอร์ต มีเพียงชื่อเรียกบริเวณกงสีนายลู่กับนางอิ้ว ว่า บ้านหนองกระสือ และในรอบๆ หนองน้ำ ก็จะมีต้นพยอม อันเป็นต้นไม้ที่สวยงามมาก เลยพลอยสูญหายไปหมดด้วย และเป็นที่มาของชื่อวัดใหม่เนินพยอม นั่นเอง
+            """,
+            'opening hours': 'Open 24 hours',
+            'phone': '062 235 0265',
+            'photos': [],
+            'reviews': [],
+        },
+        {
+            'id': 7,
+            'name': 'Bangsaen Walking Street',
+            'rating': 4.3,
+            'address': 'Bang Saen-Ang Sila, เมือง, Chon Buri District, Chon Buri 20130',
+            'details': """
+            Bangsaen Walking Street is one of most popular shopping area in Bangsaen. In 2009, it has been introduced by administrators in Bangsaen city in order to be an attractive spot for tourists and be also a new business area for Bangsaen people. Successful Bangsaen walking street come from variety leisure activities and a wide range of products offered. 
+            """,
+            'opening hours': '6PM–12AM',
+            'phone': '084 126 7727',
+            'photos': [],
+            'reviews': [],
+        },
+        {
+            'id': 8,
+            'name': 'Ramayana Water Park',
+            'rating': 4.4,
+            'address': '9 Moo 7, Na Jomtien, Pattaya, Sattahip, Chon Buri 20250',
+            'details': """
+            Ramayana Water Park — is a water park in Pattaya, which is 1.5 hours drive from Bangkok and 15 kilometers south of Pattaya City. It is one of the biggest water theme parks in Southeast Asia.
+            """,
+            'opening hours': '10AM–6PM',
+            'phone': '033 005 929',
+            'photos': [],
+            'reviews': [],
+        },
     ]
 })
 
